acafe98c396787beb4b2d0164a5a0938fa219ea8
ibrahim <ibrahim.mowafy10@gmail.com>
2022-04-04 21:40:11 +0200

    Sending a post request with new meetup

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import Card from "../components/ui/Card";
+import { useNavigate } from "react-router-dom";
 
 function NewMeetup() {
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://react-getting-started-a7e05-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((result) => {
+        navigate("/", { replace: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <section>
       <Card>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
       </Card>
     </section>
   );
